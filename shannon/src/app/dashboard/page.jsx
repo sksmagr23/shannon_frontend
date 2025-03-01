5a093ca74226e7bc6ae9d88bbcff6af305b0e7ce
sksmagr23 <sakshamag34@gmail.com>
2025-03-01 20:20:36 +0530

    changes

--- shannon/src/app/dashboard/page.jsx
+++ shannon/src/app/dashboard/page.jsx
@@ -307,7 +307,9 @@ export function Component() {
               animate={activeIndex === 3 ? "visible" : "hidden"}
               variants={pieVariants}
             >
-              <Card style={cardStyle} bodyStyle={{ padding: "24px" }}>
+              <Card
+                style={cardStyle}
+              >
                 <motion.div
                   initial={{ opacity: 0, y: -10 }}
                   animate={{ opacity: 1, y: 0 }}
